34bcd023662d757295e013228f5edcd43903a9e5
adamveris2024 <adamveris2024@gmail.com>
2025-01-16 10:03:19 -0800

    added header at the top and \t to writelines

--- COMP003A_Assignment1/Program.cs
+++ COMP003A_Assignment1/Program.cs
@@ -8,16 +8,17 @@
             string username = Console.ReadLine();
             Console.WriteLine("Hi, " + username + ". My name is Adam\n");
 
-            Console.WriteLine("1. I am a C.I.S major at Lemoore College.");
-            Console.WriteLine("2. I have experience with HTML, CSS, and JavaScript.");
-            Console.WriteLine("3. This is my second semester here at Lemoore College.");
-            Console.WriteLine("4. My current hobbies right now are playing video games or collecting Magic the Gathering cards.");
-            Console.WriteLine("5. I have a girlfriend that I have been with for almost 3 years now.");
-            Console.WriteLine("6. I plan on transferring to either Fresno State or Sacramento State.");
-            Console.WriteLine("7. My dream car is a 2020 Dodge Challenger.");
-            Console.WriteLine("8. Some genres of music I like are rap, alt-rock, R&B, and certain pop songs.");
-            Console.WriteLine("9. I love to go fishing when I can.");
-            Console.WriteLine("10. I enjoy ceramics, I hope to one day own my own potterswheel to do ceramic projects.");
+            Console.WriteLine("Here are 10 things about me:");
+            Console.WriteLine("\t1. I am a C.I.S major at Lemoore College.");
+            Console.WriteLine("\t2. I have experience with HTML, CSS, and JavaScript.");
+            Console.WriteLine("\t3. This is my second semester here at Lemoore College.");
+            Console.WriteLine("\t4. My current hobbies right now are playing video games or collecting Magic the Gathering cards.");
+            Console.WriteLine("\t5. I have a girlfriend that I have been with for almost 3 years now.");
+            Console.WriteLine("\t6. I plan on transferring to either Fresno State or Sacramento State.");
+            Console.WriteLine("\t7. My dream car is a 2020 Dodge Challenger.");
+            Console.WriteLine("\t8. Some genres of music I like are rap, alt-rock, R&B, and certain pop songs.");
+            Console.WriteLine("\t9. I love to go fishing when I can.");
+            Console.WriteLine("\t10. I enjoy ceramics, I hope to one day own my own potterswheel to do ceramic projects.");
         }
     }
 }
